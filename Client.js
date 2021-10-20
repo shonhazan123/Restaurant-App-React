0920b7f290544db4b9b420de2d899eaa54db34f1
Shon Hazan <75479393+shonhazan123@users.noreply.github.com>
2021-10-20 09:52:08 +0300

    Add files via upload

---
+++ Client.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "../assets/css/client.css";
+const Client = ({ changeMenu }) => {
+  const [clientName, setName] = useState("");
+
+  return (
+    <section className="features-boxed">
+      <div className="container text-center">
+        <h1>Regular Costumer?</h1>
+
+        <input
+          className="form-control  container text-center "
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter Your Name Here"
+        />
+        <span>
+          <button
+            disabled={!clientName}
+            className="btn btn-primary "
+            type="submit"
+            onClick={() => changeMenu(clientName.toLowerCase())}
+          >
+            Search
+          </button>
+        </span>
+      </div>
+    </section>
+  );
+};
+
+export default Client;
